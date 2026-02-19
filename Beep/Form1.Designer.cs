0bde7a1ab0948f0eb5f85aa1de3613a369e46b21
Wissididom <30803034+Wissididom@users.noreply.github.com>
2026-02-19 20:39:52 +0100

    Initial commit

---
+++ Beep/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Beep
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            BtnBeep = new Button();
+            SuspendLayout();
+            // 
+            // BtnBeep
+            // 
+            BtnBeep.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            BtnBeep.Location = new Point(12, 12);
+            BtnBeep.Name = "BtnBeep";
+            BtnBeep.Size = new Size(160, 137);
+            BtnBeep.TabIndex = 0;
+            BtnBeep.Text = "Beep";
+            BtnBeep.UseVisualStyleBackColor = true;
+            BtnBeep.Click += BtnBeep_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(184, 161);
+            Controls.Add(BtnBeep);
+            Name = "Form1";
+            Text = "Beep";
+            ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button BtnBeep;
+    }
+}
---
+++ BeepCli/Program.cs
@@ -0,0 +1,46 @@
+﻿if (OperatingSystem.IsWindows())
+{
+    if (args.Length < 2 && args.Length > 0)
+    {
+        if (args[0].Equals("help", StringComparison.OrdinalIgnoreCase))
+        {
+            Console.WriteLine($"Usage: {AppDomain.CurrentDomain.FriendlyName} <beep frequency|filepath to list of frequencies and durations separated by |, one line per beep|help> [<duration>]");
+            return;
+        }
+        var lines = File.ReadAllLines(args[0]);
+        for (int i = 0; i < lines.Length; i++)
+        {
+            var parts = lines[i].Split('|');
+            if (parts.Length < 2)
+            {
+                Console.WriteLine($"Line {i + 1} does not have 2 integers to use as frequency and duration!");
+                continue;
+            }
+            if (!int.TryParse(parts[0], out var freq))
+            {
+                Console.WriteLine($"Couldn't convert frequency of line {i + 1} to integer!");
+                continue;
+            }
+            if (!int.TryParse(parts[1], out var duration))
+            {
+                Console.WriteLine($"Couldn't convert duration of line {i + 1} to integer!");
+                continue;
+            }
+            Console.Beep(freq, duration);
+        }
+    }
+    else if (args.Length > 1)
+    {
+        Console.Beep(int.Parse(args[0]), int.Parse(args[1]));
+    }
+    else
+    {
+        Console.Beep();
+    }
+}
+else
+{
+    // TODO: Polyfill for Linux and Mac
+    Console.WriteLine("This application is only supported on Windows");
+    Console.ReadKey();
+}
